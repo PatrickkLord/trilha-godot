522b0281fadaefc786e95e1dd30e3605b54aae19
PatrickkLord <patrickkgamer66@outlook.com>
2025-05-08 16:59:18 -0400

    movimento

--- lore-swords/addons/Player/player.gd
+++ lore-swords/addons/Player/player.gd
@@ -1,15 +1,17 @@
 extends CharacterBody2D
 
+@export var speed: float = 3.0
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-var is_running:bool = false
+func _physics_process(delta: float) -> void:
+	var input_vector = Input.get_vector("mover_esquerda", "mover_direita", "mover_cima", "mover_baixo")
+	var target_velocity = input_vector * speed * 100.0
+	
+	velocity = lerp(velocity, target_velocity, 0.05)
+	move_and_slide()
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
-		if is_running:
-			animation_player.play("idle")
-			is_running = false
-		else:
-			animation_player.play("run")
-			is_running =true	
-			
+	if input_vector != Vector2.ZERO:
+		animation_player.play("run")
+	else:
+		animation_player.play("idle")
+	
