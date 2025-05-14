a20770692bd92f712c22227a9ccabc5162adc497
PatrickkLord <patrickkgamer66@outlook.com>
2025-05-14 16:53:07 -0400

    fead: created a move pawns and AI

---
+++ lore-swords/enemies/Follow.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+@export var speed = 1
+
+@onready var sprite:AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	var player_position = GameManager.player_position
+	var diference =player_position - position
+	var input_vector = diference.normalized()
+	
+	pass
+	
+	#input Vector
+	#direção
+	velocity = input_vector * speed * 100.0
+	move_and_slide()
+	
+		# Girar o sprite 	
+	if input_vector.x > 0:
+		sprite.flip_h = false
+	elif input_vector.x < 0:		
+		sprite.flip_h = true	
