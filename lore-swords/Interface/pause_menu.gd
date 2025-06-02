f560b1b243ca38205e04a2a6e52834f58fa42d30
pisi774 <pisi774@gmail.com>
2025-06-02 16:50:54 -0400

    game

--- lore-swords/Interface/pause_menu.gd
+++ lore-swords/Interface/pause_menu.gd
@@ -1,5 +1,5 @@
 extends CanvasLayer
-
+@onready var resume : Button = $Menu_holder/Resume
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	visible = false
@@ -10,18 +10,18 @@ func _process(delta: float) -> void:
 	pass
 
 
-
+func _on_resume_pressed() -> void:
+	get_tree().paused = false
+	visible = false
 
 func _on_quit_game_pressed() -> void:
 	get_tree().quit()
-
-
-
-func _on_resume_pressed() -> void:
-	get_tree()
+	
 
 
 func _unhandled_input(event):
 	if event.is_action_pressed("ui_cancel"):
 		visible = true
 		get_tree().paused = true
+		resume.grab_focus()
+		
